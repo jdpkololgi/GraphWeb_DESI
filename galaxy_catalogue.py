0311db557021b3b50312421b2a92c88e1703e36e
jdpkololgi <kololgi.dakshesh@gmail.com>
2025-05-30 03:58:35 -0700

    First commit

---
+++ galaxy_catalogue.py
@@ -0,0 +1,8 @@
+import astropy.constants as c
+import astropy.units as u
+from astropy.table import Table
+from astropy.coordinates import SkyCoord
+from astropy.io import fits
+import numpy as np
+
+import matplotlib.pyplot as plt
